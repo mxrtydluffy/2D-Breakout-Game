74ba56e8f0f0a06174931685137324f70afe0a99
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-30 00:22:19 -0800

    added README, circle & outline

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2D-Breakout-Game
+Learning JavaScript by building a classic 2D-Breakout Game
+
+## Overview
+This assignment includes functions, variables, arrays, objects, if-else statements and loops. Vanilla JavaScript will be used and then updating the tutorial to modern JS syntax using OOP.
--- main.js
+++ main.js
@@ -5,9 +5,45 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-/* Draws rectangle on the scren */
+/* 
+
+    Presents Red Rectangle
+
+    - .beginPath() | Doesn't draw anything but told context a new drawing has started since
+    its making something new.
+    - .rect(x position, y position (down from the top), width, height) | Create rectangle 
+    - fillStyle() | Adds color like adding paint. Doesn't draw anything.
+    - .fill() | Since the area on the canvas is marked, new drawing has started and define rectangle
+    and marked area on the canvas the area will be filled with the color. This will make 
+    - .closePath()| Doesn't do anything visual but it closes and from that point can start a new one.
+*/
+
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
+ctx.closePath();
+
+/* 
+    Presents green circle.
+
+    - .beginPath() | Starts a new drawing
+    - .arc(x, y, radius, startAngle, endAngle) | Draws arcs much like a compass
+        - startAngle is calcualted in radians. In degrees it's 0 - 360° in radians it's 0 - 6.28 (2 * PI)
+*/
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+
+// Draws outline of the rectangle. Similar to code above.
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+// ctx.fill() this would fill the rectangle color
 ctx.closePath();
