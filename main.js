e20fa8dc3e9dd8105b1003c26ff8c1a0f506e094
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-30 19:44:55 -0800

    Made paddle & keyboard controls

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ let dx = 2;
 let dy = -2;
 // Detects collision if ball touches the wall.
 const ballRadius = 10;
+// Defining paddle properties
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+// Boolean variables can be utilized when buttons are pressed.
+let rightPressed = false;
+let leftPressed = false;
 
 
 function drawBall() {
@@ -18,6 +25,15 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
   }
+
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+  
   
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -32,8 +48,50 @@ function drawBall() {
       dy = -dy;
     }
 
+    
+
     x += dx;
     y += dy;
+
+    /*
+    Allows the paddle to move the paddle on the screen.
+    if (rightPressed) {
+      paddleX += 7;
+    } else if (leftPressed) {
+      paddleX -= 7;
+    }
+    *** However paddle will dissapear off the edge of the canvas.
+    */
+    
+    // Determine set calculation for paddle to stay within the boudaries.
+    if (rightPressed) {
+      paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+    } else if (leftPressed) {
+      paddleX = Math.max(paddleX - 7, 0);
+    }    
+
+    drawPaddle();
   }
 
+  // EventListener listens for key presses 
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+
+  // the "key" holds information about the key that is being pressed.
+  function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+  
+  function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+  
   setInterval(draw, 10);
