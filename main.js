9cbf98badd69eb066e26f0d7c23adac83ad3848d
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-30 22:12:25 -0800

    Added collision detection

--- main.js
+++ main.js
@@ -33,32 +33,35 @@ Top and left offset so its not drawn from the edge of the canvas.
 *** CODE WILL ONLY CREATE NEW BRICKS!
 */
 const bricks = [];
+// c = brick columns
 for (let c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
+  // r = brick rows
   for (let r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0 };
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
   }
 }
 
-/*
-Function to loop via all the bricks in the array.
-*/
 
+// Function to loop via all the bricks in the array.
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
-      const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-      const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if (bricks[c][r].status === 1) {
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
+
 
 function drawBall() {
     ctx.beginPath();
@@ -82,6 +85,7 @@ function drawBall() {
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
 
     // Ball bouncing off from the left and right walls.
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -142,5 +146,25 @@ function drawBall() {
       leftPressed = false;
     }
   }
+
+  // Result in the ball changing its direction. 
+  function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+      for (let r = 0; r < brickRowCount; r++) {
+        const b = bricks[c][r];
+        if (b.status === 1) {
+          if (
+            x > b.x &&
+            x < b.x + brickWidth &&
+            y > b.y &&
+            y < b.y + brickHeight
+          ) {
+            dy = -dy;
+            b.status = 0;
+          }
+        }
+      }
+    }
+  }  
   
   const interval = setInterval(draw, 10);
