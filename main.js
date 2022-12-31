0c143749aa28102026b9d6b71007c82eab443a1f
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-30 19:17:26 -0800

    made ball bounce off the walls

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-// Starting point at the bootm center of the Canvas
+// Starting point at the bottom center of the Canvas
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 // Defining small values to make it appear the ball is moving
 let dx = 2;
 let dy = -2;
+// Detects collision if ball touches the wall.
+const ballRadius = 10;
+
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -19,23 +22,18 @@ function drawBall() {
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+
+    // Ball bouncing off from the left and right
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+    }
+    // Ball bouncing off from top edge and bottom edge 
+    if (y + dy > canvas.height || y + dy < ballRadius) {
+      dy = -dy;
+    }
+
     x += dx;
     y += dy;
   }
 
   setInterval(draw, 10);
-
-
-// function draw() {
-//     // Whole rectangle will be cleared of any content.
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx.beginPath();
-//     ctx.arc(x, y, 10, 0, Math.PI * 2);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-//     // Update x & y with dx & dy variable on every frame.
-//     // This will make the ball be painted on the new position every update.
-//     x += dx;
-//     y += dy;
-//   }
