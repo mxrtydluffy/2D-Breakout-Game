a302c771622886f943b90371ddb167cdb513cdc9
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-24 11:34:59 -0800

    did ball class

---
+++ ball.js
@@ -0,0 +1,28 @@
+// Import later to main.js
+
+class Ball {
+    constructor(x, y, radius, color, canvas) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.canvas = canvas;
+        this.dx = 2;
+        this.dy = -2;
+        this.radius = radius;
+    }
+
+    render(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, MATH.PI * 2);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+    }
+
+    move() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+  }
+
+export default Ball;
