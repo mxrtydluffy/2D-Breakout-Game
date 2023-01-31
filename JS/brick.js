15a47a3534a3c0e0a9637e94d1e0a3dc1d564902
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-31 09:59:57 -0800

    did brick class

--- JS/brick.js
+++ JS/brick.js
@@ -3,20 +3,14 @@ import Sprite from './sprite';
 class Brick extends Sprite {
     
     /*
-    - Constructor helps passes arguments to sprite
+    - #6 super helps passes arguments to sprite
+    - #12 Adds a new property
     */
-
+   
     constructor(x, y, width = 75, height = 20, color = '#0095DD') {
-        super (x, y, width, height, color);
+        super(x, y, width, height, color);
         this.status = true;
     }
-
-    // const bricks = [];
-    // const brickColumnCount = 0;
-    // const brickRowCount = 0;
-
-    // for (let c = 0; c < brickColumnCount; c += 1) {
-
-    // }
-
 }
+
+export default Brick;
