385d6535a402f7001102abcdd917d4517b114836
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-31 10:43:31 -0800

    did brick class

--- JS/brick.js
+++ JS/brick.js
@@ -11,6 +11,59 @@ class Brick extends Sprite {
         super(x, y, width, height, color);
         this.status = true;
     }
+
+    // Brick variables
+brickRowCount = 4;
+brickColumnCount = 5;
+brickWidth = 75;
+brickHeight = 20;
+brickPadding = 10;
+brickOffsetTop = 30;
+brickOffsetLeft = 30;
+
+// 
+
+bricks = [];
+// c = brick columns
+for (c = 0; c < brickColumnCount; c += 1) {
+  bricks[c] = [];
+  // r = brick rows
+  for (let r = 0; r < brickRowCount; r += 1) {
+    // Paints on 2D array
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+
+// Function to loop via all the bricks in the array.
+render(ctx) {
+    for (let c = 0; c < brickColumnCount; c += 1) {
+      for (let r = 0; r < brickRowCount; r += 1) {
+        if (bricks[c][r].status === 1) {
+          // Setting the location
+          const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+          const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+          // Setting properties of the bricks
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          // Declaring Colors
+          let brickRowColor = "#8B4411";
+          if (r === 1) {
+            brickRowColor = "#AE6E4E";
+          } else if (r === 2) {
+                brickRowColor = "#CC9767";
+          } else if (r === 3) {
+                brickRowColor = "#A57A5A";
+          }
+          ctx.fillStyle = brickRowColor;
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
 }
 
 export default Brick;
--- main.js
+++ main.js
@@ -21,21 +21,21 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
-// Brick variables
-const brickRowCount = 4;
-const brickColumnCount = 5;
-const brickWidth = 75;
-const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+// // Brick variables
+// const brickRowCount = 4;
+// const brickColumnCount = 5;
+// const brickWidth = 75;
+// const brickHeight = 20;
+// const brickPadding = 10;
+// const brickOffsetTop = 30;
+// const brickOffsetLeft = 30;
 
 // Score & lives info
 let score = 0;
 let lives = 3;
 
-// Declare Colors
-const brickColors = ["#8B4411", "#AE6E4E", "#CC9767", "#A57A5A"]
+// // Declare Colors
+// const brickColors = ["#8B4411", "#AE6E4E", "#CC9767", "#A57A5A"]
 
 /*
 Number of rows, columns, width amd height of bricks are defined. Padding is
@@ -44,16 +44,16 @@ Top and left offset so its not drawn from the edge of the canvas.
 *** CODE WILL ONLY CREATE NEW BRICKS!
 */
 
-const bricks = [];
-// c = brick columns
-for (let c = 0; c < brickColumnCount; c += 1) {
-  bricks[c] = [];
-  // r = brick rows
-  for (let r = 0; r < brickRowCount; r += 1) {
-    // Paints on 2D array
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
-  }
-}
+// const bricks = [];
+// // c = brick columns
+// for (let c = 0; c < brickColumnCount; c += 1) {
+//   bricks[c] = [];
+//   // r = brick rows
+//   for (let r = 0; r < brickRowCount; r += 1) {
+//     // Paints on 2D array
+//     bricks[c][r] = { x: 0, y: 0, status: 1 };
+//   }
+// }
 
 // Must be first
 function drawScore() {
@@ -116,41 +116,41 @@ addEventListener('keydown', keyDownHandler, false);
 addEventListener('keyup', keyUpHandler, false);
 addEventListener('mousemove', mouseMoveHandler, false);
 
-function changeBrickColor(row) {
-  if (row == 0){
-    return brickColors[0]
-  }
-  else if (row == 1) {
-    return brickColors[1]
-  }
-  else if (row == 2) {
-    return brickColors[2]
-  }
-  else if (row == 3) {
-    return brickColors[3]
-  }
-}
+// function changeBrickColor(row) {
+//   if (row == 0){
+//     return brickColors[0]
+//   }
+//   else if (row == 1) {
+//     return brickColors[1]
+//   }
+//   else if (row == 2) {
+//     return brickColors[2]
+//   }
+//   else if (row == 3) {
+//     return brickColors[3]
+//   }
+// }
 
-// Function to loop via all the bricks in the array.
-function drawBricks() {
-  for (let c = 0; c < brickColumnCount; c += 1) {
-    for (let r = 0; r < brickRowCount; r += 1) {
-      if (bricks[c][r].status === 1) {
-        // Setting the location
-        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-        // Setting properties of the bricks
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = changeBrickColor(r);
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }
-}
+// // Function to loop via all the bricks in the array.
+// function drawBricks() {
+//   for (let c = 0; c < brickColumnCount; c += 1) {
+//     for (let r = 0; r < brickRowCount; r += 1) {
+//       if (bricks[c][r].status === 1) {
+//         // Setting the location
+//         const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+//         const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+//         // Setting properties of the bricks
+//         bricks[c][r].x = brickX;
+//         bricks[c][r].y = brickY;
+//         ctx.beginPath();
+//         ctx.rect(brickX, brickY, brickWidth, brickHeight);
+//         ctx.fillStyle = changeBrickColor(r);
+//         ctx.fill();
+//         ctx.closePath();
+//       }
+//     }
+//   }
+// }
 
 // Result in the ball changing its direction.
 function collisionDetection() {
